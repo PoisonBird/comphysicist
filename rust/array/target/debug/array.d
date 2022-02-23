163aba143612f6cf8898166b707bf1be3615f07c
poisonbird <comphysicist@gmail.com>
2022-02-23 23:01:04 +0900

    update array

---
+++ rust/array/target/debug/array.d
@@ -0,0 +1,1 @@
+C:\Users\rdati\comphysicist\comphysicist\rust\array\target\debug\array.exe: C:\Users\rdati\comphysicist\comphysicist\rust\array\src\main.rs
---
+++ rust/array/target/debug/deps/array.d
@@ -0,0 +1,5 @@
+C:\Users\rdati\comphysicist\comphysicist\rust\array\target\debug\deps\array.exe: src\main.rs
+
+C:\Users\rdati\comphysicist\comphysicist\rust\array\target\debug\deps\array.d: src\main.rs
+
+src\main.rs:
